0bb76b3b6b19996212157661e0c80f8c77b7125d
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-01-29 23:58:29 +0530

    LC-print-words-vertically [Runtime: 16 ms; Memory: 12.5 MB]

---
+++ string/print-words-vertically.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def rright(self, string):
+        if(len(string) == 0 or string[-1].isalpha()):
+            return string
+            
+        return self.rright(string[:-1])
+
+    def printVertically(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        words = s.split(" ")
+        biggest_word = 0
+        for letter in words:
+            biggest_word = max(biggest_word,len(letter))
+        
+        answer = list()
+        for i in range(biggest_word):
+            verticalword = ""
+            for j in words:
+                if(len(j)>i):
+                    verticalword += j[i]
+                else:
+                    verticalword += " "
+            
+            answer.append(self.rright(verticalword))
+        return answer
+
+
+        
