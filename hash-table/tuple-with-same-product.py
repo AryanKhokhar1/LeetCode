780e2102e479a2b316e56cc57b96f2528c494802
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-06 23:16:53 +0530

    LC-tuple-with-same-product [Runtime: N/A; Memory: N/A]

---
+++ hash-table/tuple-with-same-product.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def tupleSameProduct(self, nums):
+
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        summ = dict()
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                product = nums[i] * nums[j]
+
+                if summ.get(product) == None:
+                    summ[product] = 1
+                else:
+                    summ[product] += 1
+        ans = 0
+        for v in summ.values():
+            if v > 1:
+                ans += (v//2)
+        
+        return 8*ans
