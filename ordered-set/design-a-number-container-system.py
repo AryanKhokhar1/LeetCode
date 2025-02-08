aa6d14007166bc3779f0f2b0c21028115c0fe4f0
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-08 20:14:48 +0530

    LC-design-a-number-container-system [Runtime: N/A; Memory: N/A]

--- ordered-set/design-a-number-container-system.py
+++ ordered-set/design-a-number-container-system.py
@@ -1,22 +1,17 @@
 class NumberContainers(object):
 
     def __init__(self):
-        self.lis = list()
+        self.dic = dict()
 
     def change(self, index, number):
-        if len(self.lis) < index+1 :
-            for i in range(index-(len(self.lis))):
-                self.lis.append(None)
-            self.lis.append(number)
-        else:
-            self.lis[index] = number
+        self.dic[index] = number
 
         
     def find(self, number):
-        try:
-            return self.lis.index(number)
-        except:
-            return -1
+        for k,v in self.dic.items():
+            if v == number:
+                return k
+        return -1
         
 # Your NumberContainers object will be instantiated and called as such:
 # obj = NumberContainers()
