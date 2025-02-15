7c662084cccd01b449625db144c97c8bad95f18d
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-16 00:08:20 +0530

    LC-find-the-punishment-number-of-an-integer [Runtime: 2199 ms; Memory: 12.5 MB]

--- backtracking/find-the-punishment-number-of-an-integer.py
+++ backtracking/find-the-punishment-number-of-an-integer.py
@@ -1,18 +1,19 @@
 class Solution(object):
     def punishmentNumber(self, n):
-        self.answer = 0
+        answer = 0
         for i in range(1,n+1):
             num = i**2
-            self.isPunishable(i,str(num),0,0)
-        return self.answer
+            if self.isPunishable(i,str(num),0,0):
+                answer += num
+        return answer
 
     def isPunishable(self, n, num, curr, start):
         if start >= len(num):
-            if n == curr:
-                self.answer += int(num)
-            return
+            return curr == n
         for i in range(start,len(num)):
             val = int(num[start:i+1])
 
-            self.isPunishable(n, num, curr+val,i+1)
+            if self.isPunishable(n, num, curr+val,i+1):
+                return True
+        return False
 
