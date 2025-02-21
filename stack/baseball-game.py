64da940d7ae193a49af1dde6d41464b8b2257a2e
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-21 16:36:39 +0530

    LC-baseball-game [Runtime: N/A; Memory: N/A]

---
+++ stack/baseball-game.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    score = []
+    def invalid(self):
+        if(len(self.score) > 0):
+            self.score.pop(-1)
+    def double(self):
+        if(len(self.score)>0):
+            self.score.append(self.score[-1]*2)
+    def addition(self):
+        if(len(self.score) >1):
+            self.score.append(self.score[-1]+self.score[-2])
+    def calPoints(self, operations):
+        for op in operations:
+            if op == "C":
+                self.invalid()
+            elif op == "D":
+                self.double()
+            elif op == "+":
+                self.addition()
+            else:
+                self.score.append(int(op))
+        s = 0
+        for ele in self.score:
+            s += ele
+        return s
