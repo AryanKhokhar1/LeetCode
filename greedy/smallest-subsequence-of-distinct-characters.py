f21b22d51f2456ccb798aa165c0bb2d4b2f0f5c1
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-22 13:11:27 +0530

    LC-smallest-subsequence-of-distinct-characters [Runtime: 3 ms; Memory: 12.4 MB]

---
+++ greedy/smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def smallestSubsequence(self, s):
+        freq = dict(Counter(s))
+
+        stack = []
+        in_stack = set()
+        for ch in s:
+            freq[ch] -= 1
+            if ch in in_stack:
+                continue
+
+            while stack and stack[-1] > ch and freq[stack[-1]] > 0:
+                in_stack.remove(stack.pop())
+            stack.append(ch)
+            in_stack.add(ch)     
+        return "".join(stack)
