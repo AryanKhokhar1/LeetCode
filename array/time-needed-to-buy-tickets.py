bcca755e8112b462dae983f84e305b7a0b55a5ce
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-22 18:13:10 +0530

    LC-time-needed-to-buy-tickets [Runtime: 0 ms; Memory: 12.5 MB]

--- array/time-needed-to-buy-tickets.py
+++ array/time-needed-to-buy-tickets.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def timeRequiredToBuy(self, tickets, k):
-
-        # val = tickets[k]
-        time = 0
-        while tickets[k]:
-            tickets[0] -= 1
-            if(tickets[0] > 0):
-                tickets.append(tickets.pop(0))
-                time+= 1
-                if k == 0:
-                    k = len(tickets)-1
-                else:
-                    k -= 1
+        """
+        :type tickets: List[int]
+        :type k: int
+        :rtype: int
+        """
+        ans = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                ans += min(tickets[i],tickets[k])
             else:
-                tickets.pop(0)
-                time += 1
-                if k == 0:
-                    break
-                else:
-                    k -= 1
-        return time
+                ans += min(tickets[i], tickets[k]-1)
+        return ans
