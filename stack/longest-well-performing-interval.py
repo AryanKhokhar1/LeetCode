6b454f2b52a9499fcec48da253ae1fe9ace58131
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-22 17:44:07 +0530

    LC-longest-well-performing-interval [Runtime: 27 ms; Memory: 13.3 MB]

--- stack/longest-well-performing-interval.py
+++ stack/longest-well-performing-interval.py
@@ -1,37 +1,17 @@
 class Solution(object):
     def longestWPI(self, hours):
-        prefixsum1 = []
-        prefixsum2 = []
-        
-        a , b = 0 , 0
+        result = [1 if i >8 else -1 for i in hours]
+        prefixSum = 0
+        longest_interval = 0
+        d = {}
         for i in range(len(hours)):
-            if(hours[i] > 8):
-                a += 1
-            else:
-                a -= 1
+            prefixSum += result[i]
 
-            if(hours[len(hours)-i-1] > 8):
-                b += 1
+            if prefixSum > 0:
+                longest_interval = i + 1
             else:
-                b -= 1
-            prefixsum1.append(a)
-            prefixsum2.insert(len(hours)-i-1,b)
-        
-        a = 0
-        b = 0
-        ansA = 0
-        ansB = 0
-        for ele in prefixsum1:
-            if ele > 0:
-                a += 1
-            else:
-                a = 0
-            ansA = max(a,ansA)
-        
-        for ele in prefixsum2:
-            if ele >0:
-                b += 1
-            else:
-                b = 0
-            ansB = max(b,ansB)
-        return max(ansA,ansB)
+                if prefixSum -1 in d:
+                    longest_interval = max(longest_interval, i-d[prefixSum -1])
+            if prefixSum not in d:
+                d[prefixSum] = i
+        return longest_interval
