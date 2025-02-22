13dba61ad98def8705dc097cdb99bdb97f01ffec
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-22 19:19:04 +0530

    LC-minimum-operations-to-make-binary-array-elements-equal-to-one-i [Runtime: 2449 ms; Memory: 18 MB]

---
+++ bit-manipulation/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def minOperations(self, nums):
+        operation = 0
+        for i in range(len(nums)):
+            if nums[i] == 0 and i+3 <= len(nums):
+                operation += 1 
+                nums[i] = 1
+                nums[i+1] = (0 if nums[i+1] == 1 else 1)
+                nums[i+2] = (0 if nums[i+2] == 1 else 1)
+        print(nums)
+        if nums[-1] == 0 or nums[-2] == 0:
+            return -1
+        else:
+            return operation
