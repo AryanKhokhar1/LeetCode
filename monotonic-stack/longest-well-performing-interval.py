b401c2d302baecafec64c44c08151f6bbe5c4c36
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-22 16:47:50 +0530

    LC-longest-well-performing-interval [Runtime: N/A; Memory: N/A]

--- monotonic-stack/longest-well-performing-interval.py
+++ monotonic-stack/longest-well-performing-interval.py
@@ -1,26 +1,37 @@
 class Solution(object):
     def longestWPI(self, hours):
-        result = [-1]*len(hours)
-        for i in range(len(hours)):
-            if hours[i] > 8:
-                result[i] = 1
+        prefixsum1 = []
+        prefixsum2 = []
         
-        curr = 0
-        ans = 0
-        count = 0
+        a , b = 0 , 0
         for i in range(len(hours)):
-            if hours[i] > 8:
-                curr += 1
+            if(hours[i] > 8):
+                a += 1
             else:
-                if curr > 0:
-                    curr -= 1
-                else:
-                    curr = 0
-            
-            if curr > 0:
-                count+= 1
-            else:
-                count = 0
+                a -= 1
 
-            ans = max(ans,count)
-        return ans
+            if(hours[len(hours)-i-1] > 8):
+                b += 1
+            else:
+                b -= 1
+            prefixsum1.append(a)
+            prefixsum2.insert(len(hours)-i-1,b)
+        
+        a = 0
+        b = 0
+        ansA = 0
+        ansB = 0
+        for ele in prefixsum1:
+            if ele > 0:
+                a += 1
+            else:
+                a = 0
+            ansA = max(a,ansA)
+        
+        for ele in prefixsum2:
+            if ele >0:
+                b += 1
+            else:
+                b = 0
+            ansB = max(b,ansB)
+        return max(ansA,ansB)
