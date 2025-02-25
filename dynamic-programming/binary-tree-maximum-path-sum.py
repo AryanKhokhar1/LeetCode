c365078654d7b575f5229b367efb8d1451029d06
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-25 20:29:22 +0530

    LC-binary-tree-maximum-path-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/binary-tree-maximum-path-sum.py
+++ dynamic-programming/binary-tree-maximum-path-sum.py
@@ -12,6 +12,14 @@ class Solution(object):
         leftSum = self.maxhelper(root.left , diameter)
         rightSum = self.maxhelper(root.right, diameter)
         self.diameter = max(self.diameter, (leftSum + rightSum + root.val))
+        if leftSum < 0 and rightSum < 0:
+            self.diameter = max(self.diameter, (root.val))
+        elif leftSum < 0:
+            self.diameter = max(self.diameter, (root.val + rightSum))
+        elif rightSum < 0:
+            self.diameter = max(self.diameter, (root.val + leftSum))
+        else:
+            self.diameter = max(self.diameter, (root.val + leftSum + rightSum))
         return root.val + max(leftSum , rightSum)
     def maxPathSum(self, root):
         self.diameter = -1000
