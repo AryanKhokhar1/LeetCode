0b69f205e6955e5ef7cbab0874440330dcb44254
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-02-26 22:45:05 +0530

    LC-same-tree [Runtime: 0 ms; Memory: 12.7 MB]

--- tree/same-tree.py
+++ tree/same-tree.py
@@ -5,30 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def preorder(self,root,pre):
-        if root is None:
-            return 
-        pre.append(root.val)
-        self.preorder(root.left,pre)
-        self.preorder(root.right,pre)
-        return pre
-    def inorder(self,root,ino):
-        if root is None:
-            return
-        self.inorder(root.left,ino)
-        ino.append(root.val)
-        self.inorder(root.right,ino)
-        return ino
-
-    def postorder(self,root,post):
-        if root is None:
-            return
-        self.postorder(root.left,post)
-        self.postorder(root.right,post)
-        post.append(root.val)
-        return post
+    
     def isSameTree(self, p, q):
         
-        return self.preorder(p,[]) == self.preorder(q,[]) and self.inorder(p,[]) == self.inorder(q,[]) and self.postorder(p,[]) == self.postorder(q,[])
+        if p == None or q == None:
+            return p == q
+        
+        return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right)
         
         
