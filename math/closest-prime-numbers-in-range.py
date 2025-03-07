1ea6874a2dd049d4b555db21a17a5f924ed031d8
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-03-07 21:59:57 +0530

    LC-closest-prime-numbers-in-range [Runtime: N/A; Memory: N/A]

--- math/closest-prime-numbers-in-range.py
+++ math/closest-prime-numbers-in-range.py
@@ -28,7 +28,7 @@ class Solution:
         for i in range(1, len(primes)):
             gap = primes[i] - primes[i-1]
             if gap <= 2:
-                return [primes[i], primes[i-1]]
+                return [primes[i-1], primes[i]]
             if gap < min_gap:
                 min_gap = gap
                 result = [primes[i-1], primes[i]]
