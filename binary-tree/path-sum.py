67c5b977e17c78f75fff17fe99291df52223e1ab
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-03-08 16:14:46 +0530

    LC-path-sum [Runtime: N/A; Memory: N/A]

--- binary-tree/path-sum.py
+++ binary-tree/path-sum.py
@@ -9,16 +9,21 @@ class Solution(object):
         if root is None:
             return False
         def roottoleaf(root,psum,targetSum):
-            if root is None:
+            if root.left is None and root.right is None:
                 if sum(psum) == targetSum:
                     return True
                 else:
                     return False
-            psum.append(root.val)
-
-            if roottoleaf(root.left, psum, targetSum) or roottoleaf(root.right, psum, targetSum):
-                return True
-            psum.pop()
+            if root.left:
+                psum.append(root.left.val)
+                if roottoleaf(root.left, psum,targetSum):
+                    return True
+                psum.pop()
+            if root.right:
+                psum.append(root.right.val)
+                if roottoleaf(root.right, psum, targetSum):
+                    return True
+                psum.pop()
             return False
 
         return roottoleaf(root,[],targetSum)
