894819948b0d61b9e290c3f3a84a1a2666f72340
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-03-30 22:15:03 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- enumeration/count-primes.py
+++ enumeration/count-primes.py
@@ -3,17 +3,17 @@ class Solution(object):
         if n <= 2:
             return 0
         else:
-            lis = [True]*(n-1)
+            lis = [True]*(n)
         
         for ind in range(2,len(lis)):
             if lis[ind]:
                 i = ind
-                i += i
+                i += ind
                 while(i<len(lis)):
                     lis[i] = False
-                    i+=i
+                    i+=ind
         answer = 0
-        print(lis)
+        # print(lis)
         for val in range(2,len(lis)):
             if lis[val]:
                 answer += 1
