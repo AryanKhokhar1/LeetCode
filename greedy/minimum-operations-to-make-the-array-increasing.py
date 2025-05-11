344494e2697380746c3523ea845dd006891bf280
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-05-11 15:37:35 +0530

    LC-minimum-operations-to-make-the-array-increasing [Runtime: 94 ms; Memory: 12.9 MB]

--- greedy/minimum-operations-to-make-the-array-increasing.py
+++ greedy/minimum-operations-to-make-the-array-increasing.py
@@ -3,10 +3,14 @@ class Solution(object):
         if len(nums) == 1:
             return 0
         ans = 0
-        for i in range(len(nums)-1):
-            if nums[i] >= nums[i+1]:
-                ans += (nums[i]+1) - nums[i+1]
-                nums[i+1] += (nums[i]+1) - nums[i+1]
+        num = nums[0]
+        for i in range(1,len(nums)):
+            if num >= nums[i]:
+                diff = (num+1) - nums[i]
+                ans += diff
+                num = nums[i]+ diff
+            else:
+                num = nums[i]
         return ans
 
 
