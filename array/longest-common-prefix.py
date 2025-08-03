c68b5f32641a0107ff6c18d52892f1a4ef882ccc
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-08-03 13:20:28 +0530

    LC-longest-common-prefix [Runtime: N/A; Memory: N/A]

--- array/longest-common-prefix.py
+++ array/longest-common-prefix.py
@@ -6,8 +6,15 @@ class Solution(object):
             return strs[0]
         same = strs[0]
         for ele in strs[1:]:
+            print(same, ", ", ele)
+            print(type(same),type(ele))
+            print(min(len(ele),len(same)))
             for i in range(min(len(ele),len(same))):
+                print(i)
                 if ele[i] != same[i]:
                     same = same[:i]
                     break
+                elif i == len(ele) -1:
+                    same = same[:i+1]
+            print("same = ",same)
         return same
