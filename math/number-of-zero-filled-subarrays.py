3ab4282a42505c196125ff7097d5d18550984c6d
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-08-19 14:40:53 +0530

    LC-number-of-zero-filled-subarrays [Runtime: 33 ms; Memory: 18.5 MB]

--- math/number-of-zero-filled-subarrays.py
+++ math/number-of-zero-filled-subarrays.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def zeroFilledSubarray(self, nums):
-        occurance = 0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)+1):
-                check = True
-                for ele in nums[i:j]:
-                    if ele != 0:
-                        check = False
-                        break
-                if check:
-                    occurance += 1
-        return occurance
+        answer = 0
+        occur = 0
+        for ele in nums:
+            if ele == 0:
+                occur += 1
+            else:
+                if occur > 0:
+                    answer += (occur * (occur + 1))/2
+                    occur = 0
+        if occur > 0:
+            answer += (occur * (occur + 1))/2
+        return answer
