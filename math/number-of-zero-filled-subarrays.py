34d6777f8411b611f4a9fd256cb9008fbcb0430a
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-08-19 14:28:46 +0530

    LC-number-of-zero-filled-subarrays [Runtime: N/A; Memory: N/A]

--- math/number-of-zero-filled-subarrays.py
+++ math/number-of-zero-filled-subarrays.py
@@ -3,6 +3,11 @@ class Solution(object):
         occurance = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)+1):
-                if sum(nums[i:j]) == 0:
-                    occurance += 1        
+                check = True
+                for ele in nums[i:j]:
+                    if ele != 0:
+                        check = False
+                        break
+                if check:
+                    occurance += 1
         return occurance
