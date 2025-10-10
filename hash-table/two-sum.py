3032cdad268c3f312f1ad8ff43d766ad3db85a80
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-10-10 23:47:02 +0530

    LC-two-sum [Runtime: 0 ms; Memory: 19.2 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j:
-                    if nums[i] + nums[j] == target:
-                        return [i,j]
-        return -1
+            diff = target - nums[i]
+            if diff in d:
+                return[d[diff],i]
+            d[nums[i]] = i
+        return [-1]
