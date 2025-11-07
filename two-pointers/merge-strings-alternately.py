0975235e5bf1ad147fded712725f55820d1c8c66
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-07 09:25:17 +0530

    LC-merge-strings-alternately [Runtime: 30 ms; Memory: 17.5 MB]

--- two-pointers/merge-strings-alternately.py
+++ two-pointers/merge-strings-alternately.py
@@ -2,9 +2,12 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         
         s = ""
-        for i in range(max(len(word1), len(word2))):
+        mn = min(len(word1), len(word2))
+        for i in range(mn):
             if i < len(word1):
                 s += word1[i]
             if i < len(word2):
                 s += word2[i]
+        s += word1[mn:]
+        s += word2[mn:]
         return s
