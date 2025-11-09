e28c85a87abe483fa965351d57b18d2e30de2d18
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-09 21:33:14 +0530

    LC-count-operations-to-obtain-zero [Runtime: 99 ms; Memory: 34.6 MB]

--- simulation/count-operations-to-obtain-zero.py
+++ simulation/count-operations-to-obtain-zero.py
@@ -1,11 +1,10 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        noperation = 0
-        while num1 != 0 and num2 != 0:
-            if num1 < num2:
-                num2 = num2 - num1
+        if num1 == 0 or num2 == 0:
+            return 0
+        else:
+            if num1 > num2:
+                return 1 + self.countOperations(num1-num2, num2)
             else:
-                num1 = num1 - num2
-            noperation += 1
-        return noperation
+                return 1 + self.countOperations(num1,num2-num1)
         
