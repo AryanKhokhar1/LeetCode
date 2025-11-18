8711a3dbd7c53ecab28dab77b647c9654f16e262
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-18 11:21:16 +0530

    LC-count-negative-numbers-in-a-sorted-matrix [Runtime: 0 ms; Memory: 19.1 MB]

--- array/count-negative-numbers-in-a-sorted-matrix.py
+++ array/count-negative-numbers-in-a-sorted-matrix.py
@@ -1,8 +1,21 @@
 class Solution:
     def countNegatives(self, grid: List[List[int]]) -> int:
-        ans = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] < 0:
-                    ans += 1
-        return ans
+        count = 0
+
+        def binarySearch(arr):
+            start = 0
+            end = len(arr)-1
+            while(start<=end):
+                mid = start + (end - start) //2
+                if arr[mid] >= 0:
+                    start = mid + 1
+                else:
+                    end = mid -1
+            if start == len(arr):
+                return 0
+            if end == -1:
+                return len(arr)
+            return len(arr) - start
+        for arr in grid:
+            count += binarySearch(arr)
+        return count 
