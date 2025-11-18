4b26631dc76b2dee84ca5b6bd405511bd09a5af9
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-18 08:20:48 +0530

    LC-1-bit-and-2-bit-characters [Runtime: 0 ms; Memory: 17.8 MB]

--- array/1-bit-and-2-bit-characters.py
+++ array/1-bit-and-2-bit-characters.py
@@ -5,23 +5,15 @@ class Solution:
         # invalid representation 1, 01
         if bits[-1] != 0:
             return False
-        valid = True
-        both = True
         i = 1
         while(i<len(bits)):
-            print("i: ",i,"length of bits: ",len(bits))
             if bits[i-1] == 0:
-                print(bits[i-1])
                 i+=1
             elif bits[i-1] == 1:
-                print(bits[i-1])
-                print("i: ",i,"length of bits: ",len(bits))
                 if i < len(bits)-1:
                     if bits[i] == 1:
-                        print(bits[i])
                         i+=2
                     else:
-                        print(bits[i])
                         i+=2
                 else:
                     return False
