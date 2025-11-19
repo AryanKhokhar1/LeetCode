f3e9cdc779da0b84702084107dd545a558fae895
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-19 08:18:45 +0530

    LC-keep-multiplying-found-values-by-two [Runtime: 0 ms; Memory: 18.1 MB]

--- hash-table/keep-multiplying-found-values-by-two.py
+++ hash-table/keep-multiplying-found-values-by-two.py
@@ -1,11 +1,13 @@
 class Solution:
     def findFinalValue(self, nums: List[int], original: int) -> int:
+        d = dict()
+        for ele in nums:
+            if d.get(ele) == None:
+                d[ele] = True
         found = True
         while(found):
-            found = False
-            for ele in nums:
-                if ele == original:
-                    original *= 2
-                    found = True
-                    break
+            if d.get(original) == True:
+                original *= 2
+            else:
+                found = False
         return original
