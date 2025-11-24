a8e21a892ec3c21faf85a74c72a13424b2bbac23
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-24 22:25:44 +0530

    LC-average-value-of-even-numbers-that-are-divisible-by-three [Runtime: 0 ms; Memory: 17.9 MB]

--- math/average-value-of-even-numbers-that-are-divisible-by-three.py
+++ math/average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -1,7 +1,11 @@
 class Solution:
     def averageValue(self, nums: List[int]) -> int:
         ans = 0
+        n = 0
         for ele in nums:
             if ele %6 == 0:
                 ans += ele
-        return ans //2
+                n+= 1
+        if n == 0:
+            return 0
+        return ans // n
