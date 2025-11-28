43726529f5657969138ab85a8eec5a33fa7c06c7
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-11-28 09:59:24 +0530

    LC-sort-an-array [Runtime: N/A; Memory: N/A]

---
+++ merge-sort/sort-an-array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return self.divide(nums)
+    
+    def divide(self,arr):
+        if len(arr) == 1:
+            return arr
+
+        mid = len(arr)//2
+        left = self.divide(arr[:mid])
+        right = self.divide(arr[mid:])
+        return self.merge(left,right)
+    
+    def merge(self,left,right):
+        newarr = []
+
+        i = 0
+        j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                newarr.append(left[i])
+                i+=1
+            else:
+                newarr.append(right[j])
+                j+=1
+        while i<len(left):
+            newarr.append(left[i])
+            i += 1
+        while j < len(right):
+            newarrr.append(right[j])
+            j += 1
+        return newarr
