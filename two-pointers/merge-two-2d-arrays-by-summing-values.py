777fa8aabf9a0dc314491da5e360c086f85bb582
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-12-04 10:17:49 +0530

    LC-merge-two-2d-arrays-by-summing-values [Runtime: 0 ms; Memory: 18.2 MB]

--- two-pointers/merge-two-2d-arrays-by-summing-values.py
+++ two-pointers/merge-two-2d-arrays-by-summing-values.py
@@ -1,24 +1,26 @@
-class Solution(object):
-    def mergeArrays(self, nums1, nums2):
+class Solution:
+    def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        
         i = 0
         j = 0
-        nums = []
-        while(i < len(nums1) and j < len(nums2)):
-            if nums1[i][0] == nums2[j][0]:
-                nums.append([nums1[i][0],nums1[i][1]+nums2[j][1]])
-                i += 1
-                j += 1
-            elif nums1[i][0] > nums2[j][0]:
-                nums.append(nums2[j])
-                j += 1
-            else:
-                nums.append(nums1[i])
+        ans = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] < nums2[j][0]:
+                ans.append(nums1[i])
                 i+=1
-        while i<len(nums1):
-            nums.append(nums1[i])
+            elif nums1[i][0] > nums2[j][0]:
+                ans.append(nums2[j])
+                j+=1
+            else:
+                ans.append([nums1[i][0],nums1[i][1]+nums2[j][1]])
+                i+=1
+                j+=1
+        while(i<len(nums1)):
+            ans.append(nums1[i])
             i+=1
-        while j<len(nums2):
-            nums.append(nums2[j])
+        
+        while j < len(nums2):
+            ans.append(nums2[j])
             j+=1
-        return nums
-
+        
+        return ans
