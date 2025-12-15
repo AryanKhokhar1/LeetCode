e7152f8b35da63031fdbcd8b163882d0e4bcc191
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2025-12-15 09:33:53 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- sorting/merge-sorted-array.py
+++ sorting/merge-sorted-array.py
@@ -1,15 +1,16 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        curr = len(nums1)-1
-        m -= 1
-        n -= 1
-        while n >= 0 and curr >= 0:
-            if nums1[m] < nums2[n]:
-                nums1[curr] = nums2[n]
-                n -=1
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        curr = (m+n)-1
+        n1 = m-1
+        n2 = n-1
+        while(n2>=0):
+            if nums1[n1] >= nums2[n2]:
+                nums1[curr],nums1[n1] = nums1[n1],nums1[curr]
+                n1 -= 1
             else:
-                extra = nums1[curr]
-                nums1[curr] = nums1[m]
-                nums1[m] = extra
-                m -=1
-            curr -=1
+                nums1[curr] = nums2[n2]
+                n2 -= 1
+            curr -= 1
