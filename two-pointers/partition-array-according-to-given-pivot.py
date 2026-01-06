95944b2398e8b657e09f92ab70aa48655795bbf4
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2026-01-06 11:05:56 +0530

    LC-partition-array-according-to-given-pivot [Runtime: N/A; Memory: N/A]

--- two-pointers/partition-array-according-to-given-pivot.py
+++ two-pointers/partition-array-according-to-given-pivot.py
@@ -1,13 +1,16 @@
-class Solution(object):
-    def pivotArray(self, nums, pivot):
-        leftNums = []
-        rightNums = []
-        pivotNums = []
-        for ele in nums:
-            if ele < pivot:
-                leftNums.append(ele)
-            elif ele > pivot:
-                rightNums.append(ele)
-            else:
-                pivotNums.append(ele)
-        return leftNums + pivotNums + rightNums
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        for i in range(len(nums),0,-1):
+            for j in range(1,i):
+                print(nums)
+                if nums[j] == pivot and nums[j-1] == pivot or nums[j] < pivot and nums[j-1] < pivot or nums[j] > pivot and nums[j-1] > pivot:
+                    print("Same Category")
+                    continue
+                else:
+                    print("Different Category",end=" ")
+                    print(f"nums[j] = {nums[j]} and nums[j-1] = {nums[j-1]}")
+                    if nums[j] < nums[j-1]:
+                        nums[j],nums[j-1] = nums[j-1],nums[j]
+                    
+        print(nums)
+        return nums
