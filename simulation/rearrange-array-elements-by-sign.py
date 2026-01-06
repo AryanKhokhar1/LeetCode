bb087e1ef6083205722903284762b9968bd1ed65
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2026-01-06 11:47:32 +0530

    LC-rearrange-array-elements-by-sign [Runtime: 41 ms; Memory: 43.6 MB]

--- simulation/rearrange-array-elements-by-sign.py
+++ simulation/rearrange-array-elements-by-sign.py
@@ -1,14 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
         # with the help of two pointer approach
-        i,j = 0,1
-        while i < len(nums):
-            if nums[i] >= 0:
-                i += 2
-            elif nums[j] < 0:
+        i = 0
+        j = 1
+        ans = [0]*len(nums)
+        for ele in nums:
+            if ele > 0:
+                ans[i] = ele
+                i +=2
+            elif ele < 0:
+                ans[j] = ele
                 j += 2
-            else:
-                nums[i],nums[j] = nums[j],nums[i]
-                i+=2
-                j+=2
-        return nums
+        return ans 
+    
