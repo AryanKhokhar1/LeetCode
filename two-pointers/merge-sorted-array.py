7ba16df5a3cbdb790d1f11f04f2d0c3a369e267e
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2026-03-14 08:36:23 +0530

    LC-merge-sorted-array [Runtime: N/A; Memory: N/A]

--- two-pointers/merge-sorted-array.py
+++ two-pointers/merge-sorted-array.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        curr = (m+n)-1
-        n1 = m-1
-        n2 = n-1
-        while(n2>=0):
-            if nums1[n1] >= nums2[n2]:
-                nums1[curr],nums1[n1] = nums1[n1],nums1[curr]
-                n1 -= 1
-            else:
-                nums1[curr] = nums2[n2]
-                n2 -= 1
-            curr -= 1
+        curr = m
+        for ele in nums2:
+            for i in range(curr,-1,-1):
+                print(nums1, )
+                if ele <= nums1[i-1]:
+                    nums1[i] , nums1[i-1] = nums1[i-1], nums1[i]
+                else: 
+                    nums1[i] = ele
+                    curr +=1
+                    break
+        
