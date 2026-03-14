7e3c2cf281d787ed029ed2a00486a770c1ff82ee
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2026-03-14 09:34:21 +0530

    LC-how-many-numbers-are-smaller-than-the-current-number [Runtime: 0 ms; Memory: 19.3 MB]

--- sorting/how-many-numbers-are-smaller-than-the-current-number.py
+++ sorting/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,16 +1,11 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        count = [0]*(max(nums)+1)
+        sortnums = sorted(nums)
+        d = dict()
         for i in range(len(nums)):
-            count[nums[i]] += 1
-
-        for i in range(1,len(count)):
-            count[i] += count[i-1]
-        
-        answer = []
+            if d.get(sortnums[i]) == None:
+                d[sortnums[i]] = i
+        ans = []
         for ele in nums:
-            if ele != 0:
-                answer.append(count[ele-1])
-            else:
-                answer.append(0)
-        return answer
+            ans.append(d.get(ele))
+        return ans
