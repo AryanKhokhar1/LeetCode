886dea6c4db35c9f2deeab46560305c882db0b94
Aryan Khokhar <117306452+AryanKhokhar1@users.noreply.github.com>
2026-06-25 01:23:21 +0530

    LC-two-sum [Runtime: N/A; Memory: N/A]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,9 +1,3 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in d:
-                return[d[diff],i]
-            d[nums[i]] = i
-        return [-1]
+        
